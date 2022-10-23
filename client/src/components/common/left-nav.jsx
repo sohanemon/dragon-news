c190247343f81469eb1ddf28bfa00af59953aa1a
sohanemon <m.sohanemon@gmail.com>
2022-10-23 22:10:56 +0600

    env updated

--- client/src/components/common/left-nav.jsx
+++ client/src/components/common/left-nav.jsx
@@ -4,7 +4,7 @@ import { Link, NavLink } from "react-router-dom";
 const LeftNav = () => {
   const [categories, setCategories] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:5000/categories")
+    fetch(`${process.env.REACT_APP_HOST}/categories`)
       .then((res) => res.json())
       .then((data) => setCategories(data));
 
